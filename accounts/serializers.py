543df1ba194e1ba27a6f73785b2ecc4807597fd4
Ahm455 <Ahmedgoharkiyani@gmail.com>
2026-04-15 12:55:54 +0500

    clean

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -8,3 +8,13 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['username','password','clerk_id','first_name','last_name','full_name',
                   'email','phone_number','is_headmaster','is_teacher','is_student','semester','date_joined','date_of_birth']
 
+    def validate(self, data):
+        roles = [
+            data.get("is_student", False),
+            data.get("is_teacher", False),
+            data.get("is_headmaster", False),]
+
+        if sum(roles) > 1:
+            raise serializers.ValidationError("Only one role can be true at a time.")
+
+        return data
